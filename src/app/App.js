92bfc8ebe9c796ca1b4204016acc0dabf39f2c4f
Charles Young <cbyoung415@gmail.com>
2022-08-09 08:46:17 -0700

    add components

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-
-
-
+import { Routes, Route } from "react-router-dom";
+import Home from "../components/Home/Home";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css';
+
+
+
+
+export const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home;
