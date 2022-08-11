5e1eadf2fc644b597d8e0bf5cb398cac8443c9a1
Charles Young <cbyoung415@gmail.com>
2022-08-11 12:00:39 -0700

    add a form toggle for mobile users

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Hero.css";
 import { FaDatabase, FaAsterisk, FaAccusoft } from "react-icons/fa";
 import SignInForm from "../Form/SignInForm";
 
 const Hero = () => {
+  const [formActive, setFormActive] = useState("");
+
+  const handleClick = () => {
+    if (formActive !== "active") {
+      setFormActive("active");
+    } else {
+      setFormActive("");
+    }
+  };
+
   return (
     <div className="hero">
       <div className="container">
@@ -32,9 +42,12 @@ const Hero = () => {
               </div>
             </div>
           </div>
-          <div className="col-2">
+          <div className={`col-2 ${formActive}`}>
             <SignInForm />
           </div>
+          <button className={`login-bttn ${formActive}`} onClick={handleClick}>
+            {formActive !== "active" ? " Sign up/Login" : "Close"}
+          </button>
         </div>
       </div>
     </div>
