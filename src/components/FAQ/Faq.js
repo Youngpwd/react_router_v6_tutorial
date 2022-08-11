a81dbfbb8f6e33545ce3d663828df971d6dac890
Charles Young <cbyoung415@gmail.com>
2022-08-11 12:02:06 -0700

    initialize faq files

---
+++ src/components/FAQ/Faq.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Faq.css";
+
+const Faq = () => {
+  return (
+    <div className="section-faq">
+      <div className="container">
+        <div className="col-1">
+          <h3>Frequently asked questions</h3>
+          <p>Can't find the answer you're looking for?</p>
+          <p>
+            Reach out to our <span>customer support</span> team.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Faq;
