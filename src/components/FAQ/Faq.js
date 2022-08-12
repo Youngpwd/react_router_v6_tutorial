a3e9c91c31d6fd99b127aa7cd3cd8c4d037fbfc4
Charles Young <cbyoung415@gmail.com>
2022-08-12 08:06:33 -0700

    faq jsx

--- src/components/FAQ/Faq.js
+++ src/components/FAQ/Faq.js
@@ -12,6 +12,30 @@ const Faq = () => {
             Reach out to our <span>customer support</span> team.
           </p>
         </div>
+        <div className="col-2">
+          <div className="faq">
+            <h5>Are Aliens real?</h5>
+            <p>Was from are flatterers of his, to mothernot thence in.</p>
+          </div>
+          <div className="faq">
+            <h5>Is the earth flat or round?</h5>
+            <p>
+              Carnal since this ah he know ever mirth dome later in. Call known
+              had few.
+            </p>
+          </div>
+          <div className="faq">
+            <h5>How does computers computer computer things?</h5>
+            <p>
+              Utters more uttered of of of such for door, muttered throws into
+              and sculptured so the have. But the the.
+            </p>
+          </div>
+          <div className="faq">
+            <h5>Does holy water work on demons?</h5>
+            <p>Dolores accusam nonumy no sed ipsum est sanctus, clita stet.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
