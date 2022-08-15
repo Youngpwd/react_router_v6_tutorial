769c9c0b1d9c129c809cafa266ee8598ff59b822
Charles Young <cbyoung415@gmail.com>
2022-08-15 09:10:42 -0700

    add Routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../components/Home/Home";
+import ContactPage from "../pages/ContactPage";
+import FaqPage from "../pages/FaqPage";
+import PricingPage from "../pages/PricingPage";
 
 function App() {
+  const [formActive, setFormActive] = useState("");
+  const [menuOn, setMenuOn] = useState(false);
+
+  const handleForm = () => {
+    if (formActive !== "active") {
+      setFormActive("active");
+    } else {
+      setFormActive("");
+    }
+  };
+
+  const handleMenu = () => {
+    setMenuOn(!menuOn);
+  };
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              handleForm={handleForm}
+              handleMenu={handleMenu}
+              menuOn={menuOn}
+              formActive={formActive}
+            />
+          }
+        />
+        <Route
+          path="/pricing"
+          element={<PricingPage handleMenu={handleMenu} menuOn={menuOn} />}
+        />
+        <Route path="/faq" element={<FaqPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </>
   );
